57501b3cbb4bb8c4a87e8bc9662806d4cae659a1
kolluru sridhar <87171968+sridharshree303@users.noreply.github.com>
2021-11-10 10:49:50 +0530

    Initial commit

---
+++ src/main/java/com/cg/spring/core/demo/App.java
@@ -0,0 +1,14 @@
+package com.cg.spring.core.demo;
+
+/*
+ * @author Vaman deshmukh 
+ * @Hello world program
+ *  
+ */
+
+public class App {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/com/cg/spring/core/demo/AppTest.java
@@ -0,0 +1,13 @@
+package com.cg.spring.core.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}

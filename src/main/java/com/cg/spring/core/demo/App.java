f84f85d4034e30c0abdfc9a2f6696e089dbfaccc
kolluru sridhar <87171968+sridharshree303@users.noreply.github.com>
2021-11-10 12:47:04 +0530

    c

--- src/main/java/com/cg/spring/core/demo/App.java
+++ src/main/java/com/cg/spring/core/demo/App.java
@@ -1,7 +1,12 @@
 package com.cg.spring.core.demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.cg.spring.core.demo.model.Employee;
+
 /*
- * @author Vaman deshmukh spring project 
+ * @author Vaman Deshmukh spring demo project 
  * @Hello world program
  *  
  */
@@ -9,6 +14,16 @@ package com.cg.spring.core.demo;
 public class App {
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		System.out.println("Start");
+		
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("SpringConfig.xml");
+		
+	//	Employee emp = new Employee(101,"sonu",10.7);
+		
+		Employee emp = ctx.getBean(Employee.class);
+		
+		System.out.println(emp.toString());
+		
+		System.out.println("End");
 	}
 }
